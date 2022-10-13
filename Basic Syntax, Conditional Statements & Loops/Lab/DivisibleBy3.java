d84fbd21a96be0ffc0bb300dbe730d3451072646
Merian <drundrun00@gmail.com>
2022-10-13 16:36:05 +0300

    Added exercise

---
+++ Basic Syntax, Conditional Statements & Loops/Lab/DivisibleBy3.java
@@ -0,0 +1,13 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class DivisibleBy3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 3; i <= 99; i += 3) {
+            System.out.println(i);
+        }
+    }
+}
