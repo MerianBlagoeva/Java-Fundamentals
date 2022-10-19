4ad2356608c4c69bc7782dd0ed3b1c68f7d8bbf3
Merian <drundrun00@gmail.com>
2022-10-19 21:10:35 +0300

    Added exercise

---
+++ Basic Syntax, Conditional Statements & Loops/Lab/PassesOrFailed.java
@@ -0,0 +1,18 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class PassesOrFailed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        double grade = Double.parseDouble(sc.nextLine());
+
+        if (grade >= 3) {
+            System.out.println("Passed!");
+        } else {
+            System.out.println("Failed!");
+        }
+    }
+}
