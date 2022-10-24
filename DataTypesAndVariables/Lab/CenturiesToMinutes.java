b8157f909225e8545127268bebebcf4991a9ad07
Merian <drundrun00@gmail.com>
2022-10-24 19:13:08 +0300

    Added topic and exercise

---
+++ DataTypesAndVariables/Lab/CenturiesToMinutes.java
@@ -0,0 +1,19 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class CenturiesToMinutes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int centuries = Integer.parseInt(sc.nextLine());
+        int years = centuries * 100;
+        double days = years * 365.2422;
+        double hours = days * 24;
+        double minutes = hours * 60;
+
+        System.out.printf("%d centuries = %d years = %.0f days = %.0f hours = %.0f minutes",
+                centuries, years, days, hours, minutes);
+    }
+}
